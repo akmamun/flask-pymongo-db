e81d175993c24da7f29af10b4f21b7ec0f72fc18
Al Mamun Khan <almamunkhan04@gmail.com>
2018-10-22 23:11:09 +0600

    Update db.py

--- db.py
+++ db.py
@@ -2,5 +2,5 @@ from flask_pymongo import PyMongo
 import server as s
 
 s.app.config['MONGO_DBNAME'] = 'db_name'
-s.app.config['MONGO_URI'] = 'mongodb://<db_user>:<db_password>@ds261429.mlab.com:61429/conntect_to_mongo'
+s.app.config['MONGO_URI'] = 'mongodb://<db_user>:<db_password>@ds261429.mlab.com:61429/table_name_of_db'
 mongo = PyMongo(s.app)
